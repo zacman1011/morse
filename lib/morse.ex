f2be957b9189d158c055bf6b30e00469feab3df7
Zac <zacseymour968@googlemail.com>
2024-02-24 23:24:04 +0000

    to morse

--- lib/morse.ex
+++ lib/morse.ex
@@ -6,48 +6,48 @@ defmodule Morse do
   @blank " "
   @dot "."
   @dash "-"
-  @available_characters [@dot, @dash]
+  @available_characters [@dot, @dash, @blank]
 
   @acc_starter ""
   @current_starter ""
 
   @conversion [
-    {"a", "#{@dot}#{@dash}"},
-    {"b", "#{@dash}#{@dot}#{@dot}#{@dot}"},
-    {"c", "#{@dash}#{@dot}#{@dash}#{@dot}"},
-    {"d", "#{@dash}#{@dot}#{@dot}"},
+    {"a", "#{@dot} #{@dash}"},
+    {"b", "#{@dash} #{@dot} #{@dot} #{@dot}"},
+    {"c", "#{@dash} #{@dot} #{@dash} #{@dot}"},
+    {"d", "#{@dash} #{@dot} #{@dot}"},
     {"e", "#{@dot}"},
-    {"f", "#{@dot}#{@dot}#{@dash}#{@dot}"},
-    {"g", "#{@dash}#{@dash}#{@dot}"},
-    {"h", "#{@dot}#{@dot}#{@dot}#{@dot}"},
-    {"i", "#{@dot}#{@dot}"},
-    {"j", "#{@dot}#{@dash}#{@dash}#{@dash}"},
-    {"k", "#{@dash}#{@dot}#{@dash}"},
-    {"l", "#{@dot}#{@dash}#{@dot}#{@dot}"},
-    {"m", "#{@dash}#{@dash}"},
-    {"n", "#{@dash}#{@dot}"},
-    {"o", "#{@dash}#{@dash}#{@dash}"},
-    {"p", "#{@dot}#{@dash}#{@dash}#{@dot}"},
-    {"q", "#{@dash}#{@dash}#{@dot}#{@dash}"},
-    {"r", "#{@dot}#{@dash}#{@dot}"},
-    {"s", "#{@dot}#{@dot}#{@dot}"},
+    {"f", "#{@dot} #{@dot} #{@dash} #{@dot}"},
+    {"g", "#{@dash} #{@dash} #{@dot}"},
+    {"h", "#{@dot} #{@dot} #{@dot} #{@dot}"},
+    {"i", "#{@dot} #{@dot}"},
+    {"j", "#{@dot} #{@dash} #{@dash} #{@dash}"},
+    {"k", "#{@dash} #{@dot} #{@dash}"},
+    {"l", "#{@dot} #{@dash} #{@dot} #{@dot}"},
+    {"m", "#{@dash} #{@dash}"},
+    {"n", "#{@dash} #{@dot}"},
+    {"o", "#{@dash} #{@dash} #{@dash}"},
+    {"p", "#{@dot} #{@dash} #{@dash} #{@dot}"},
+    {"q", "#{@dash} #{@dash} #{@dot} #{@dash}"},
+    {"r", "#{@dot} #{@dash} #{@dot}"},
+    {"s", "#{@dot} #{@dot} #{@dot}"},
     {"t", "#{@dash}"},
-    {"u", "#{@dot}#{@dot}#{@dash}"},
-    {"v", "#{@dot}#{@dot}#{@dot}#{@dash}"},
-    {"w", "#{@dot}#{@dash}#{@dash}"},
-    {"x", "#{@dash}#{@dot}#{@dot}#{@dash}"},
-    {"y", "#{@dash}#{@dot}#{@dash}#{@dash}"},
-    {"z", "#{@dash}#{@dash}#{@dot}#{@dot}"},
-    {"1", "#{@dot}#{@dash}#{@dash}#{@dash}#{@dash}"},
-    {"2", "#{@dot}#{@dot}#{@dash}#{@dash}#{@dash}"},
-    {"3", "#{@dot}#{@dot}#{@dot}#{@dash}#{@dash}"},
-    {"4", "#{@dot}#{@dot}#{@dot}#{@dot}#{@dash}"},
-    {"5", "#{@dot}#{@dot}#{@dot}#{@dot}#{@dot}"},
-    {"6", "#{@dash}#{@dot}#{@dot}#{@dot}#{@dot}"},
-    {"7", "#{@dash}#{@dash}#{@dot}#{@dot}#{@dot}"},
-    {"8", "#{@dash}#{@dash}#{@dash}#{@dot}#{@dot}"},
-    {"9", "#{@dash}#{@dash}#{@dash}#{@dash}#{@dot}"},
-    {"0", "#{@dash}#{@dash}#{@dash}#{@dash}#{@dash}"}
+    {"u", "#{@dot} #{@dot} #{@dash}"},
+    {"v", "#{@dot} #{@dot} #{@dot} #{@dash}"},
+    {"w", "#{@dot} #{@dash} #{@dash}"},
+    {"x", "#{@dash} #{@dot} #{@dot} #{@dash}"},
+    {"y", "#{@dash} #{@dot} #{@dash} #{@dash}"},
+    {"z", "#{@dash} #{@dash} #{@dot} #{@dot}"},
+    {"1", "#{@dot} #{@dash} #{@dash} #{@dash} #{@dash}"},
+    {"2", "#{@dot} #{@dot} #{@dash} #{@dash} #{@dash}"},
+    {"3", "#{@dot} #{@dot} #{@dot} #{@dash} #{@dash}"},
+    {"4", "#{@dot} #{@dot} #{@dot} #{@dot} #{@dash}"},
+    {"5", "#{@dot} #{@dot} #{@dot} #{@dot} #{@dot}"},
+    {"6", "#{@dash} #{@dot} #{@dot} #{@dot} #{@dot}"},
+    {"7", "#{@dash} #{@dash} #{@dot} #{@dot} #{@dot}"},
+    {"8", "#{@dash} #{@dash} #{@dash} #{@dot} #{@dot}"},
+    {"9", "#{@dash} #{@dash} #{@dash} #{@dash} #{@dot}"},
+    {"0", "#{@dash} #{@dash} #{@dash} #{@dash} #{@dash}"}
   ]
 
   @letters_morse Map.new(@conversion)
@@ -76,15 +76,17 @@ defmodule Morse do
     ## End of letter so add letter and continue to next letter
     convert_morse(rest, @current_starter, "#{acc}#{convert_morse_letter(current)}")
   end
-  defp convert_morse([@blank | rest], current, acc) do
-    ## Space between characters so ignore and carry on building letter
-    convert_morse(rest, current, acc)
-  end
+  # defp convert_morse([@blank | rest], current, acc) do
+  #   ## Space between characters so ignore and carry on building letter
+  #   convert_morse(rest, current, acc)
+  # end
   defp convert_morse([next | rest], current, acc) when next in @available_characters do
+    ## Where the next grapheme is acceptable, append it to current to add to the current letter build
     convert_morse(rest, "#{current}#{next}", acc)
   end
 
   defp convert_morse_letter(""), do: ""
+  defp convert_morse_letter(" "), do: ""
   defp convert_morse_letter(letter) do
     case Map.fetch(@morse_letters, letter) do
       {:ok, letter} ->
@@ -92,6 +94,46 @@ defmodule Morse do
 
       :error ->
         throw("Invalid combination does not convert to letter: #{inspect letter}")
+    end
+  end
+
+  @doc """
+    Converts plaintext to morse code
+  """
+  @spec to_morse(String.t()) :: String.t()
+  def to_morse(morse) do
+    morse
+    |> String.graphemes()
+    |> convert_plaintext()
+  end
+
+  defp convert_plaintext(graphemes, last \\ nil, acc \\ "")
+  defp convert_plaintext([], _last, acc) do
+    ## Finished conversion so trim spaces and return
+    String.trim(acc)
+  end
+  defp convert_plaintext([" " | rest], _last, acc) do
+    ## If space then we have reached the end of a word so add the end of word marker (7 spaces)
+    convert_plaintext(rest, " ", "#{acc}       ")
+  end
+  defp convert_plaintext([next | rest], last, acc) do
+    ## Add the 3 spaces that mark end of a letter if last was not a space or the start and then add the morse conversion for the letter
+    spacing = if is_nil(last) or last == " " do
+      ""
+    else
+      "   "
+    end
+    letter = convert_letter(next)
+    convert_plaintext(rest, letter, "#{acc}#{spacing}#{letter}")
+  end
+
+  defp convert_letter(letter) do
+    case Map.fetch(@letters_morse, letter) do
+      {:ok, morse} ->
+        morse
+
+      :error ->
+        throw("Invalid character that cannot be converted to morse code: #{inspect letter}")
     end
   end
 
--- test/morse_test.exs
+++ test/morse_test.exs
@@ -70,7 +70,65 @@ defmodule MorseTest do
     end
 
     test "Illegal combination throws error" do
-      assert catch_throw(Morse.from_morse("#{@dash} #{@dot} #{@dot} #{@dot} #{@dash}")) == "Invalid combination does not convert to letter: \"-...-\""
+      assert catch_throw(Morse.from_morse("#{@dash} #{@dot} #{@dot} #{@dot} #{@dash}")) == "Invalid combination does not convert to letter: \"- . . . -\""
+    end
+
+  end
+
+  describe "to_morse/1" do
+
+    test "Single letter" do
+      assert Morse.to_morse("a") == "#{@dot} #{@dash}"
+      assert Morse.to_morse("b") == "#{@dash} #{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("c") == "#{@dash} #{@dot} #{@dash} #{@dot}"
+      assert Morse.to_morse("d") == "#{@dash} #{@dot} #{@dot}"
+      assert Morse.to_morse("e") == "#{@dot}"
+      assert Morse.to_morse("f") == "#{@dot} #{@dot} #{@dash} #{@dot}"
+      assert Morse.to_morse("g") == "#{@dash} #{@dash} #{@dot}"
+      assert Morse.to_morse("h") == "#{@dot} #{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("i") == "#{@dot} #{@dot}"
+      assert Morse.to_morse("j") == "#{@dot} #{@dash} #{@dash} #{@dash}"
+      assert Morse.to_morse("k") == "#{@dash} #{@dot} #{@dash}"
+      assert Morse.to_morse("l") == "#{@dot} #{@dash} #{@dot} #{@dot}"
+      assert Morse.to_morse("m") == "#{@dash} #{@dash}"
+      assert Morse.to_morse("n") == "#{@dash} #{@dot}"
+      assert Morse.to_morse("o") == "#{@dash} #{@dash} #{@dash}"
+      assert Morse.to_morse("p") == "#{@dot} #{@dash} #{@dash} #{@dot}"
+      assert Morse.to_morse("q") == "#{@dash} #{@dash} #{@dot} #{@dash}"
+      assert Morse.to_morse("r") == "#{@dot} #{@dash} #{@dot}"
+      assert Morse.to_morse("s") == "#{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("t") == "#{@dash}"
+      assert Morse.to_morse("u") == "#{@dot} #{@dot} #{@dash}"
+      assert Morse.to_morse("v") == "#{@dot} #{@dot} #{@dot} #{@dash}"
+      assert Morse.to_morse("w") == "#{@dot} #{@dash} #{@dash}"
+      assert Morse.to_morse("x") == "#{@dash} #{@dot} #{@dot} #{@dash}"
+      assert Morse.to_morse("y") == "#{@dash} #{@dot} #{@dash} #{@dash}"
+      assert Morse.to_morse("z") == "#{@dash} #{@dash} #{@dot} #{@dot}"
+    end
+
+    test "Single number" do
+      assert Morse.to_morse("1") == "#{@dot} #{@dash} #{@dash} #{@dash} #{@dash}"
+      assert Morse.to_morse("2") == "#{@dot} #{@dot} #{@dash} #{@dash} #{@dash}"
+      assert Morse.to_morse("3") == "#{@dot} #{@dot} #{@dot} #{@dash} #{@dash}"
+      assert Morse.to_morse("4") == "#{@dot} #{@dot} #{@dot} #{@dot} #{@dash}"
+      assert Morse.to_morse("5") == "#{@dot} #{@dot} #{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("6") == "#{@dash} #{@dot} #{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("7") == "#{@dash} #{@dash} #{@dot} #{@dot} #{@dot}"
+      assert Morse.to_morse("8") == "#{@dash} #{@dash} #{@dash} #{@dot} #{@dot}"
+      assert Morse.to_morse("9") == "#{@dash} #{@dash} #{@dash} #{@dash} #{@dot}"
+      assert Morse.to_morse("0") == "#{@dash} #{@dash} #{@dash} #{@dash} #{@dash}"
+    end
+
+    test "Single word" do
+      assert Morse.to_morse("sos") == "#{@dot} #{@dot} #{@dot}   #{@dash} #{@dash} #{@dash}   #{@dot} #{@dot} #{@dot}"
+    end
+
+    test "Multiple words" do
+      assert Morse.to_morse("hello world") == "#{@dot} #{@dot} #{@dot} #{@dot}   #{@dot}   #{@dot} #{@dash} #{@dot} #{@dot}   #{@dot} #{@dash} #{@dot} #{@dot}   #{@dash} #{@dash} #{@dash}       #{@dot} #{@dash} #{@dash}   #{@dash} #{@dash} #{@dash}   #{@dot} #{@dash} #{@dot}   #{@dot} #{@dash} #{@dot} #{@dot}   #{@dash} #{@dot} #{@dot}"
+    end
+
+    test "Illegal combination throws error" do
+      assert catch_throw(Morse.to_morse("@")) == "Invalid character that cannot be converted to morse code: \"@\""
     end
 
   end
